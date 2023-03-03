16c076cffc7f9e59de68c38a9b57481aa9df9ae5
Relizc <3464840574@qq.com>
2023-03-03 21:12:11 +0800

    Updated the login page

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 window.onload = () => {
-    if(localStorage.getItem("token")==null){
-        window.location = "/login/index.html"
-    }
+    // if(localStorage.getItem("token")==null){
+    //     window.location = "/login/index.html"
+    // }
+}
+
+function whoops(msg, errid) {
+    
 }
--- scripts/login.js
+++ scripts/login.js
@@ -34,4 +34,31 @@ function getRandomInt(min, max) {
 
 window.onload = () => {
     document.body.style = "background-image: url(\"/assets/wp" + getRandomInt(0, 2) + ".jpg\"); background-size: cover; background-repeat: no-repeat;"
+    document.getElementById("logger").onclick = () => {
+        document.getElementById("logger").innerHTML = `<i class="fa fa-circle-o-notch fa-spin"></i>`
+        document.getElementById("logger").disabled = true;
+        document.getElementById("username").disabled = true;
+
+        xml = new XMLHttpRequest;
+        xml.onreadystatechange = () =>{
+            if (xml.readystate != 4) return;
+
+            if (xml.status == 404) {
+                alert("Wrong username or password")
+            }
+
+            else if(xml.status == 200){
+                alert("Success!");
+                parsed = JSON.parse(xml.responseText);
+
+                localStorage.setItem("token",parsed["token"])
+            }
+        }
+        xml.open("POST","https://LostnFoundAPI.abra6325.repl.co/api/login");
+
+        xml.send(JSON.stringify({
+            "username":userName,
+            "password":password
+        }))
+    }
 }
